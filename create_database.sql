72b6b629446b7ea13433e6914e4fd5ec52b8fb86
Maia Iglesias Rojas <iglesiasmaia6@gmail.com>
2022-08-31 21:30:50 -0300

    Delete create_database.sql

--- create_database.sql
+++
@@ -1,8 +0,0 @@
--- Create a database named tyrell_corp.
-CREATE DATABASE IF NOT EXISTS tyrell_corp;
-CREATE TABLE IF NOT EXISTS nexus6 (
-	name VARCHAR(256), 
-	id INT
-);
-INSERT INTO nexus6 (name, id) VALUES ("Leon", 1);
-GRANT SELECT ON tyrell_corp.nexus6 TO 'holberton_user'@'localhost'
