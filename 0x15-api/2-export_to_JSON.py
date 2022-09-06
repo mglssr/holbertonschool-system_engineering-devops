cb2cd8980c7a47508f0781ddca4c0f2394b4e4a7
mglssr <iglesiasmaia6@gmail.com>
2022-09-06 20:28:07 +0000

    task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Using what you did in the task #0, extend your
+Python script to export data in the JSON format."""
+
+
+if __name__ == "__main__":
+    import json
+    import requests
+    import sys
+
+    user_id = sys.argv[1]
+    url_employee = "https://jsonplaceholder.typicode.com/users/{}"\
+        .format(user_id)
+    url_todo = "https://jsonplaceholder.typicode.com/todos?userId={}"\
+        .format(user_id)
+    req_employee = requests.get(url_employee)
+    req_todo = requests.get(url_todo)
+    username = req_employee.json().get('username')
+    filename = "{}.json".format(user_id)
+    user = {}
+    user_list = []
+    with open(filename , "w", encoding="utf-8") as _file:
+        for data in req_todo.json():
+            dic = {"task": data['title'], "completed": data['completed'], "username": username}
+            user_list.append(dic)
+        user = {'{}'.format(user_id): user_list}
+        _file.write(json.dumps(user))
